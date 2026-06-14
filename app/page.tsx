a8dfedbf03834ab2ec66136dd04c2ef5bb954edb
lacendary <lacendary@gmail.com>
2026-06-13 20:18:41 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,35 @@
-export default function Home() {
+import { gql } from "graphql-request";
+import { client } from "./lib/wordpress";
+
+async function getSneakers() {
+  const query = gql`
+    query GetSneakers {
+      sneakers {
+        nodes {
+          title
+        }
+      }
+    }
+  `;
+
+  const data = await client.request(query);
+  return data;
+}
+
+export default async function Home() {
+  const data = await getSneakers();
+
   return (
     <main style={{ padding: "2rem" }}>
       <h1>Lacendary Kicks</h1>
-      <p>The sneaker encyclopedia.</p>
+
+      <h2>Sneakers</h2>
+
+      <ul>
+        {data.sneakers.nodes.map((sneaker: any) => (
+          <li key={sneaker.title}>{sneaker.title}</li>
+        ))}
+      </ul>
     </main>
   );
 }
