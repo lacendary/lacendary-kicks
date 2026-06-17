37012a88d4aaf4378c8ca1cfffe4bceace7b2a10
lacendary <lacendary@gmail.com>
2026-06-16 22:35:04 -0500

    Add spinner component

---
+++ components/SneakerSpinner.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useState } from "react";
+
+export default function SneakerSpinner({
+  images,
+}: {
+  images: { sourceUrl: string }[];
+}) {
+  const [frame, setFrame] = useState(0);
+
+  return (
+    <div>
+      <img
+        src={images[frame].sourceUrl}
+        alt="360 View"
+        className="w-full rounded-xl shadow-2xl"
+      />
+
+      <input
+        type="range"
+        min="0"
+        max={images.length - 1}
+        value={frame}
+        onChange={(e) =>
+          setFrame(Number(e.target.value))
+        }
+        className="w-full mt-4"
+      />
+    </div>
+  );
+}
